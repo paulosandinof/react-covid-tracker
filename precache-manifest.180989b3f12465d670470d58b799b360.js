d03a7642ceee69f6d1285d2d4932ebe64f2494a0
Paulo Sandino <paulosandino@hotmail.com>
2020-04-29 00:01:53 -0300

    Updates

---
+++ precache-manifest.180989b3f12465d670470d58b799b360.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13b4603d58ebe005347f246feb8711ff",
+    "url": "/react-covid-tracker/index.html"
+  },
+  {
+    "revision": "9432b0339e2c3366959d",
+    "url": "/react-covid-tracker/static/css/main.ca591bfb.chunk.css"
+  },
+  {
+    "revision": "f3a708a35caeb9c68d04",
+    "url": "/react-covid-tracker/static/js/2.d5764273.chunk.js"
+  },
+  {
+    "revision": "44e39e61fb6a0450f7c8014318774752",
+    "url": "/react-covid-tracker/static/js/2.d5764273.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9432b0339e2c3366959d",
+    "url": "/react-covid-tracker/static/js/main.bb953f4c.chunk.js"
+  },
+  {
+    "revision": "e69d67ab6b71686b8d94",
+    "url": "/react-covid-tracker/static/js/runtime-main.65dbcbcf.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/react-covid-tracker/static/media/image.d7265326.png"
+  }
+]);
